9d69be746d82a2c684621d2202e8c5199fdcdf0a
John <john.suero@fiserv.com>
2023-11-02 13:05:40 -0400

    sumList done

--- Source.java
+++ Source.java
@@ -11,7 +11,17 @@ class Node<T> {
 class Source {
   public static int sumList(Node<Integer> head) {
     // todo
-    return;
+
+    int sum = 0;
+    while (head != null) {
+      //System.out.println(head.val);
+      sum += head.val;
+      head = head.next;
+  }
+
+
+
+    return sum;
   }
 
   public static void main(String[] args) {
